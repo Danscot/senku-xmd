7c1b1219419bf3cb366550058fc40ae9eb626c95
Daniel Scotfield <99121218+Danscot@users.noreply.github.com>
2025-11-11 22:28:26 +0100

    Update connect.js

--- utils/connect.js
+++ utils/connect.js
@@ -1,5 +1,4 @@
-import pkg from 'baileys';
-const { makeWASocket, useMultiFileAuthState, DisconnectReason} = pkg;
+import { makeWASocket, useMultiFileAuthState, DisconnectReason} from 'baileys';
 import configManager from '../utils/manageConfigs.js';
 
 import fs from "fs";
